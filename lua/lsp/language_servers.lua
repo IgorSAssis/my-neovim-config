a96ba2a82a146193e162c372e821549d462d5912
IgorIA <igorsemphoski@gmail.com>
2022-03-09 21:32:02 -0300

    Configuring nvim-lsp-installer

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,81 +1,21 @@
--- Setup lspconfig.
-local configs = require'lspconfig.configs'
-local lspconfig = require'lspconfig'
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+local lsp_installer = require('nvim-lsp-installer')
 
-local lang_servers = {
-   "html",
-   "cssls",
-   "tsserver",
-   "bashls",
-   "clangd",
-   "cmake",
-   "dockerls",
-   "phpactor",
-   "jedi_language_server",
-   "jsonls",
-   "rust_analyzer",
-   "sqlls",
-   "ls_emmet"
-}
+lsp_installer.on_server_ready(function(server)
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
+                                                                       .make_client_capabilities())
+  local opts = {capabilities = capabilities}
+  if server.name == "sumneko_lua" then
+    opts = vim.tbl_deep_extend("force", {
+      settings = {
+        Lua = {
+          runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+          diagnostics = {globals = {'vim'}},
+          workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
+          telemetry = {enable = false}
+        }
+      }
 
-lspconfig.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-
-if not configs.ls_emmet then
-  configs.ls_emmet = {
-    default_config = {
-      cmd = { 'ls_emmet', '--stdio' };
-      filetypes = {
-        'html',
-        'css',
-        'scss',
-        'javascript',
-        'javascriptreact',
-        'typescript',
-        'typescriptreact',
-        'haml',
-        'xml',
-        'xsl',
-        'pug',
-        'slim',
-        'sass',
-        'stylus',
-        'less',
-        'sss',
-        'hbs',
-        'handlebars',
-      };
-      root_dir = function(fname)
-        return vim.loop.cwd()
-      end;
-      settings = {};
-    };
-  }
-end
-
-for _, server in ipairs(lang_servers) do
-  require'lspconfig'[server].setup {
-    capabilities = capabilities
-  }
-end
+    }, opts)
+  end
+  server:setup(opts)
+end)
