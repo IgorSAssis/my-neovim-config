8dd55a6295e64290d4871e82b0bde19c8e6ec4aa
IgorIA <igorsemphoski@gmail.com>
2022-03-09 21:03:28 -0300

    Changing toggleterm configurations

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -1,10 +1,17 @@
 require'toggleterm'.setup {
-    size = 13,
-    open_mapping = [[<C-t>]],
-    shade_filetypes = {},
-    shade_terminals = true,
-    shading_factor = '1',
-    start_in_insert = true,
-    persist_size = true,
-    direction = 'horizontal',
+  size = 13,
+  open_mapping = [[<C-t>]],
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = '1',
+  start_in_insert = true,
+  persist_size = true,
+  direction = 'float',
+  float_opts = {
+    border = 'double',
+    width = 140,
+    height = 35,
+    winblend = 3,
+    highlights = {border = "Normal", background = "Normal"}
+  }
 }
